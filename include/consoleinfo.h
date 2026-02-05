b80dbe855cde95a5fa2da2cdf3f36cbaeb138d7b
Red Bees <redbeesraae@gmail.com>
2026-02-05 00:17:45 -0500

    init

---
+++ include/consoleinfo.h
@@ -0,0 +1,127 @@
+#define IS_CAFE (*(vu16*)0xcd8005a0 == 0xCAFE)
+
+#define HW_CHIPREVID (*(vu32*)0xcd800214)
+#define LT_CHIPREVID (*(vu8*)0xcd8005a3)
+
+#define PVR_LONESTAR_DD20   0x00080200 // 750L
+#define PVR_LONESTAR_DD22   0x00088202
+
+#define PVR_GEKKO_DD10      0x70000100
+#define PVR_GEKKO_DD20      0x00080100 // Also Lonestar DD1 apparently
+#define PVR_GEKKO_DD23      0x00083213
+#define PVR_GEKKO_DD23a     0x00083203
+#define PVR_GEKKO_DD23b     PVR_GEKKO_DD23 // why??
+#define PVR_GEKKO_DD23e     PVR_GEKKO_DD23
+#define PVR_GEKKO_DD23ei    PVR_GEKKO_DD23a 
+#define PVR_GEKKO_DD24      0x00083204
+#define PVR_GEKKO_DD24e     0x00083214
+#define PVR_GEKKO_DD40      0x00083410 
+// 2.3=2.3b=2.3e, 2.3a=2.3ei
+
+#define PVR_BROADWAY_BASE   0x00087000
+#define PVR_BROADWAY_DD10   0x00087100
+#define PVR_BROADWAY_DD101  0x00087110
+#define PVR_BROADWAY_DD12   0x00087102
+#define PVR_BROADWAY_DD12i  0x00087112
+#define PVR_BROADWAY_DD13   0x00087103
+#define PVR_BROADWAY_DD13i  0x00087113
+#define PVR_BROADWAY_DD20   0x00087200
+
+#define  HOLLYWOOD_ES1_0      0x00000000      // ES1.00
+#define  HOLLYWOOD_ES1_1      0x00000001      // ES1.10
+#define  HOLLYWOOD_ES1_2      0x00000002      // ES1.21
+#define  HOLLYWOOD_ES2_0      0x00000010      // ES2.01
+#define  HOLLYWOOD_ES2_1      0x00000011      // ES2.11
+#define  HOLLYWOOD_ES3_0      0x00000020      // ES3.0
+#define  HOLLYWOOD_ES3_1      0x00000021      // ES3.1
+
+typedef enum {
+	ConsoleType_GameCube = 0,
+	ConsoleType_Wii,
+	ConsoleType_WiiU,
+	ConsoleType_Count
+} ConsoleType;
+
+const char *console_type_str[ConsoleType_Count] = {
+	"Nintendo GameCube",
+	"Wii",
+	"Wii U"
+};
+
+typedef enum {
+	CpuType_LonestarDD20 = 0,
+	CpuType_LonestarDD22,
+	CpuType_GekkoDD10,
+	CpuType_GekkoDD20,
+	CpuType_GekkoDD23,
+	CpuType_GekkoDD23a,
+	CpuType_GekkoDD24,
+	CpuType_GekkoDD24e,
+	CpuType_GekkoDD40,
+	CpuType_BroadwayDD10,
+	CpuType_BroadwayDD101,
+	CpuType_BroadwayDD12,
+	CpuType_BroadwayDD12i,
+	CpuType_BroadwayDD13,
+	CpuType_BroadwayDD13i,
+	CpuType_BroadwayDD20,
+	CpuType_Espresso,
+	CpuType_Unknown,
+	CpuType_Count
+} CpuType;
+
+const char *cpu_type_str[CpuType_Count] = {
+	"PowerPC 750L DD2.0 (Lonestar)",
+	"PowerPC 750L DD2.2 (Lonestar)",
+	"Gekko DD1.0",
+	"Gekko DD2.0 (or Lonestar DD1.0)",
+	"Gekko DD2.3 (or 2.3b/e)",
+	"Gekko DD2.3a (or 2.3ei)",
+	"Gekko DD2.4",
+	"Gekko DD2.4e",
+	"Gekko DD4.0",
+	"Broadway DD1.0",
+	"Broadway DD1.01",
+	"Broadway DD1.2",
+	"Broadway DD1.2i",
+	"Broadway DD1.3",
+	"Broadway DD1.3i",
+	"Broadway DD2.0", // Broadway-1
+	"Espresso",
+	"Unknown"
+};
+
+typedef enum {
+	ChipsetType_FlipperRevA = 0,
+	ChipsetType_FlipperRevB,
+	ChipsetType_FlipperRevC,
+	ChipsetType_HollywoodES10,
+	ChipsetType_HollywoodES11,
+	ChipsetType_HollywoodES12,
+	ChipsetType_HollywoodES20,
+	ChipsetType_HollywoodES21,
+	ChipsetType_HollywoodES30, // bollywood
+	ChipsetType_HollywoodES31,
+	ChipsetType_Unknown,
+	ChipsetType_Count
+} ChipsetType;
+
+const char *chipset_type_str[ChipsetType_Count] = {
+	"Flipper (rev A)",
+	"Flipper (rev B)",
+	"Flipper (rev C)",
+	"Hollywood ES1.0",
+	"Hollywood ES1.1",
+	"Hollywood ES1.21",
+	"Hollywood ES2.01",
+	"Hollywood ES2.1",
+	"Hollywood ES3.0 (Bollywood ES)",
+	"Hollywood ES3.1 (Bollywood)",
+	"Unknown"
+};
+
+struct console_info {
+	char console_type[64];
+	char cpu_type[64];
+	char chipset_type[64];
+};
---
+++ src/main.c
@@ -0,0 +1,190 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <gccore.h>
+#include <string.h>
+#include <ogc/machine/processor.h>
+#include <wiiuse/wpad.h>
+#include "consoleinfo.h"
+
+#define VERSION_MAJOR 0
+#define VERSION_MINOR 1
+
+static void *xfb = NULL;
+static GXRModeObj *rmode = NULL;
+
+int get_console_type( void ) {
+	if(IS_CAFE) 
+		return ConsoleType_WiiU;
+	else
+		return ConsoleType_Wii;
+}
+
+int get_cpu_type(u32 pvr) {
+	switch(pvr) {
+		case PVR_LONESTAR_DD20:
+			return CpuType_LonestarDD20;
+		case PVR_LONESTAR_DD22:
+			return CpuType_LonestarDD22;
+
+		case PVR_GEKKO_DD10:
+			return CpuType_GekkoDD10;
+		case PVR_GEKKO_DD20: // or lonestar dd1
+			return CpuType_GekkoDD20;
+		case PVR_GEKKO_DD23: // or b or e
+			return CpuType_GekkoDD23;
+		case PVR_GEKKO_DD23a: // or ei
+			return CpuType_GekkoDD23a;
+		case PVR_GEKKO_DD24:
+			return CpuType_GekkoDD24;
+		case PVR_GEKKO_DD24e:
+			return CpuType_GekkoDD24e;
+		case PVR_GEKKO_DD40:
+			return CpuType_GekkoDD40;
+
+		case PVR_BROADWAY_DD10:
+			return CpuType_BroadwayDD10;
+		case PVR_BROADWAY_DD101:
+			return CpuType_BroadwayDD101;
+		case PVR_BROADWAY_DD12:
+			return CpuType_BroadwayDD12;
+		case PVR_BROADWAY_DD12i:
+			return CpuType_BroadwayDD12i;
+		case PVR_BROADWAY_DD13:
+			return CpuType_BroadwayDD13;
+		case PVR_BROADWAY_DD13i:
+			return CpuType_BroadwayDD13i;
+		case PVR_BROADWAY_DD20:
+			return CpuType_BroadwayDD20;
+		default:
+			return CpuType_Unknown;
+		}
+}
+
+int get_chipset_type( void ) {
+	if (!IS_CAFE) {
+		switch(HW_CHIPREVID) {
+			case HOLLYWOOD_ES1_0:
+				return ChipsetType_HollywoodES10;
+			case HOLLYWOOD_ES1_1:
+				return ChipsetType_HollywoodES11;
+			case HOLLYWOOD_ES1_2:
+				return ChipsetType_HollywoodES12;
+			case HOLLYWOOD_ES2_0:
+				return ChipsetType_HollywoodES20;
+			case HOLLYWOOD_ES2_1:
+				return ChipsetType_HollywoodES21;
+			case HOLLYWOOD_ES3_0:
+				return ChipsetType_HollywoodES30;
+			case HOLLYWOOD_ES3_1:
+				return ChipsetType_HollywoodES31;
+			default:
+				return ChipsetType_Unknown;
+		}
+	}
+	return ChipsetType_Unknown;
+}
+
+void get_console_info(struct console_info* c_ptr) {
+	// 3 phases:
+	// Variables are retrieved by helper function
+	// Strings are permuted for special cases
+	// Strings are copied into struct
+	u32 pvr = mfpvr();
+	int ctype_index = get_console_type();
+	int cputype_index = get_cpu_type(pvr);
+	int chipset_type_index = get_chipset_type();
+	char cpu_type[64];
+	char console_type[64];
+	char chipset_type[64];
+	
+	strcpy(console_type, console_type_str[ctype_index]);
+	strcpy(cpu_type, cpu_type_str[cputype_index]);
+	strcpy(chipset_type, chipset_type_str[chipset_type_index]);
+	
+	if(cputype_index == CpuType_Unknown) {
+		if((pvr & PVR_BROADWAY_BASE) == 0x87000) {
+			sprintf(cpu_type, "maybe Broadway DD%d.%d", 
+			((pvr & 0x00000f00) >> 8), (pvr & 0x0000000f));
+		}
+	}
+	
+	strcpy(c_ptr->console_type, console_type);
+	strcpy(c_ptr->cpu_type, cpu_type);
+	strcpy(c_ptr->chipset_type, chipset_type);
+}
+
+void libogc_init( void ) {
+	//---------------------------------------------------------------------------------
+
+	// Initialise the video system
+	VIDEO_Init();
+
+	// This function initialises the attached controllers
+	WPAD_Init();
+
+	// Obtain the preferred video mode from the system
+	// This will correspond to the settings in the Wii menu
+	rmode = VIDEO_GetPreferredMode(NULL);
+
+	// Allocate memory for the display in the uncached region
+	xfb = MEM_K0_TO_K1(SYS_AllocateFramebuffer(rmode));
+
+	// Initialise the console, required for printf
+	console_init(xfb,20,20,rmode->fbWidth,rmode->xfbHeight,rmode->fbWidth*VI_DISPLAY_PIX_SZ);
+
+	// Set up the video registers with the chosen mode
+	VIDEO_Configure(rmode);
+
+	// Tell the video hardware where our display memory is
+	VIDEO_SetNextFramebuffer(xfb);
+
+	// Make the display visible
+	VIDEO_SetBlack(false);
+
+	// Flush the video register changes to the hardware
+	VIDEO_Flush();
+
+	// Wait for Video setup to complete
+	VIDEO_WaitVSync();
+	if(rmode->viTVMode&VI_NON_INTERLACE) VIDEO_WaitVSync();
+
+
+	// The console understands VT terminal escape codes
+	// This positions the cursor on row 2, column 0
+	// we can use variables for this with format codes too
+	// e.g. printf ("\x1b[%d;%dH", row, column );
+	printf("\x1b[2;0H");
+}
+
+int main(int argc, char **argv) {
+	libogc_init();
+	
+	struct console_info c;
+	struct console_info* c_ptr;
+	c_ptr = &c;
+
+	get_console_info(c_ptr);
+	 
+	printf("Spout v%i.%i by RedBees - raregamingdump.ca\n", VERSION_MAJOR, VERSION_MINOR);
+	printf("Console Type: %s\n", c.console_type);
+	printf("CPU: %s\n", c.cpu_type);
+	printf("Chipset: %s\n", c.chipset_type);
+
+	while(SYS_MainLoop()) {
+
+		// Call WPAD_ScanPads each loop, this reads the latest controller states
+		WPAD_ScanPads();
+
+		// WPAD_ButtonsDown tells us which buttons were pressed in this loop
+		// this is a "one shot" state which will not fire again until the button has been released
+		u32 pressed = WPAD_ButtonsDown(0);
+
+		// We return to the launcher application via exit
+		if ( pressed & WPAD_BUTTON_HOME ) exit(0);
+
+		// Wait for the next frame
+		VIDEO_WaitVSync();
+	}
+
+	return 0;
+}
